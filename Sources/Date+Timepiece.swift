903c4614e898c5076aba06a2f324ef631f8212d8
Naoto Kaneko <naoty.k@gmail.com>
2016-10-15 21:22:21 +0900

    Extend Date initializers

---
+++ Sources/Date+Timepiece.swift
@@ -0,0 +1,66 @@
+//
+//  Date+Timepiece.swift
+//  Timepiece
+//
+//  Created by Naoto Kaneko on 10/2/16.
+//  Copyright © 2016 Naoto Kaneko. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    /// Creates a new instance with specified date components.
+    ///
+    /// - parameter era:        The era.
+    /// - parameter year:       The year.
+    /// - parameter month:      The month.
+    /// - parameter day:        The day.
+    /// - parameter hour:       The hour.
+    /// - parameter minute:     The minute.
+    /// - parameter second:     The second.
+    /// - parameter nanosecond: The nanosecond.
+    /// - parameter calendar:   The calendar used to create a new instance.
+    ///
+    /// - returns: The created `Date` instance.
+    public init(era: Int?, year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int, nanosecond: Int, on calendar: Calendar) {
+        let now = Date()
+        var dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second, .nanosecond], from: now)
+        dateComponents.era = era
+        dateComponents.year = year
+        dateComponents.month = month
+        dateComponents.day = day
+        dateComponents.hour = hour
+        dateComponents.minute = minute
+        dateComponents.second = second
+        dateComponents.nanosecond = nanosecond
+
+        let date = calendar.date(from: dateComponents)
+        self.init(timeInterval: 0, since: date!)
+    }
+
+    /// Creates a new instance with specified date componentns.
+    ///
+    /// - parameter year:       The year.
+    /// - parameter month:      The month.
+    /// - parameter day:        The day.
+    /// - parameter hour:       The hour.
+    /// - parameter minute:     The minute.
+    /// - parameter second:     The second.
+    /// - parameter nanosecond: The nanosecond. `0` by default.
+    ///
+    /// - returns: The created `Date` instance.
+    public init(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int, nanosecond: Int = 0) {
+        self.init(era: nil, year: year, month: month, day: day, hour: hour, minute: minute, second: second, nanosecond: nanosecond, on: .current)
+    }
+
+    /// Creates a new Instance with specified date components
+    ///
+    /// - parameter year:  The year.
+    /// - parameter month: The month.
+    /// - parameter day:   The day.
+    ///
+    /// - returns: The created `Date` instance.
+    public init(year: Int, month: Int, day: Int) {
+        self.init(year: year, month: month, day: day, hour: 0, minute: 0, second: 0)
+    }
+}
---
+++ Tests/Date+TimepieceTests.swift
@@ -0,0 +1,55 @@
+//
+//  Date+TimepieceTests.swift
+//  Timepiece
+//
+//  Created by Naoto Kaneko on 10/15/16.
+//  Copyright © 2016 Naoto Kaneko. All rights reserved.
+//
+
+import Timepiece
+import XCTest
+
+class DateTests: XCTestCase {
+    func testInit() {
+        let heisei = 235
+        var japaneseCalendar = Calendar(identifier: .japanese)
+        japaneseCalendar.timeZone = TimeZone(identifier: "Asia/Tokyo")!
+        let date = Date(era: heisei, year: 26, month: 8, day: 15, hour: 20, minute: 25, second: 43, nanosecond: 0, on: japaneseCalendar)
+
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = TimeZone(identifier: "GMT")!
+        let dateComponents = calendar.dateComponents([.year, .month, .day, .hour, .minute, .second, .nanosecond, .weekday], from: date)
+        XCTAssertEqual(dateComponents.year, 2014)
+        XCTAssertEqual(dateComponents.month, 8)
+        XCTAssertEqual(dateComponents.day, 15)
+        XCTAssertEqual(dateComponents.hour, 11)
+        XCTAssertEqual(dateComponents.minute, 25)
+        XCTAssertEqual(dateComponents.second, 43)
+        XCTAssertEqual(dateComponents.nanosecond, 0)
+        XCTAssertEqual(dateComponents.weekday, 6)
+    }
+
+    func testInitForTime() {
+        let date = Date(year: 2014, month: 8, day: 15, hour: 20, minute: 25, second: 43)
+
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second, .nanosecond, .weekday], from: date)
+        XCTAssertEqual(dateComponents.year, 2014)
+        XCTAssertEqual(dateComponents.month, 8)
+        XCTAssertEqual(dateComponents.day, 15)
+        XCTAssertEqual(dateComponents.hour, 20)
+        XCTAssertEqual(dateComponents.minute, 25)
+        XCTAssertEqual(dateComponents.second, 43)
+        XCTAssertEqual(dateComponents.nanosecond, 0)
+        XCTAssertEqual(dateComponents.weekday, 6)
+    }
+
+    func testInitForDate() {
+        let date = Date(year: 2014, month: 8, day: 15)
+
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second, .nanosecond, .weekday], from: date)
+        XCTAssertEqual(dateComponents.year, 2014)
+        XCTAssertEqual(dateComponents.month, 8)
+        XCTAssertEqual(dateComponents.day, 15)
+        XCTAssertEqual(dateComponents.weekday, 6)
+    }
+}

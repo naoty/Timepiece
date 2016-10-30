6e166630e6b06eb507cf008cabe3ef4e65ab1a7e
Naoto Kaneko <naoty.k@gmail.com>
2016-10-30 11:12:39 +0900

    Add missing documentations

--- Sources/String+Timepiece.swift
+++ Sources/String+Timepiece.swift
@@ -9,6 +9,11 @@
 import Foundation
 
 extension String {
+    /// Creates a `Date` instance representing the receiver parsed into `Date` in a given format.
+    ///
+    /// - parameter format: The format to be used to parse.
+    ///
+    /// - returns: The created `Date` instance.
     public func date(inFormat format: String) -> Date? {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = format
@@ -18,6 +23,11 @@ extension String {
         return dateFormatter.date(from: self)
     }
 
+    /// Creates a `Date` instance representing the receiver in ISO8601 format parsed into `Date` with given options.
+    ///
+    /// - parameter options: The options to be used to parse.
+    ///
+    /// - returns: The created `Date` instance.
     @available(iOS 10.0, *)
     public func dateInISO8601Format(withOptions options: ISO8601DateFormatter.Options = [.withInternetDateTime]) -> Date? {
         let dateFormatter = ISO8601DateFormatter()

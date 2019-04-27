fd20ff2bca65cd642f40886af2949f41f8643fa2
woxtu <woxtup@gmail.com>
2019-04-28 00:46:02 +0900

    Update to Swift 5

--- Package.swift
+++ Package.swift
@@ -1,5 +1,25 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-    name: "Timepiece"
+    name: "Timepiece",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+    ],
+    products: [
+        .library(name: "Timepiece", targets: ["Timepiece"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Timepiece",
+            dependencies: [],
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "TimepieceTests",
+            dependencies: ["Timepiece"]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
 )
